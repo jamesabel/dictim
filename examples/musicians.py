ec3bb4a55c99de06fb80104d4aa00e1cc5a748cc
JamesAbel <j@abel.co>
2024-12-26 14:45:45 -0800

    expand example

--- examples/musicians.py
+++ examples/musicians.py
@@ -4,12 +4,23 @@ from dictim import dictim
 
 
 def musicians():
-    favorites = dictim({"Hamm": "bass", "Skolnick": "guitar", "Björk": "vocal", "MOTÖRHEAD": "band"})
-    # access values with case insensitivity, including unicode
-    print(favorites["hamm"])
-    print(favorites["skolnick"])
-    print(favorites["björk"])
-    print(favorites["Motörhead"])
+    # keys with various cases (values remain the same)
+    favorites = dictim({"Hamm": "bass", "Skolnick": "guitar", "Björk": "vocal", "MOTÖRHEAD": "band", "Anne-Sophie Mutter": "Violin", "Strauß": "composer"})
+    print("given:")
+    for artist, kind in favorites.items():
+        print(f" {artist}: {kind}")
+    print()
+
+    # access values with different cases than the original dictim, including Unicode
+    print("using different case keys than the given mapping:")
+    for artist in ["HAMM", "skolnick", "BJÖRK", "MotörHead", "Anne-sophie MUTTER", "StrauSS"]:
+        kind = favorites[artist]
+        print(f" {artist}: {kind}")
+    print()
+
+    print('"in" is also case-insensitive and case fold:')
+    print(f' {"haMM" in favorites=}')
+    print(f' {"Strauss" in favorites=}')
 
 
 if is_main():
