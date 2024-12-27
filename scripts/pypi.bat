5832e1dc74ab53a373d3b6fc446772f8846ebc76
JamesAbel <j@abel.co>
2024-12-26 17:42:14 -0800

    clean up temp dirs

--- scripts/pypi.bat
+++ scripts/pypi.bat
@@ -7,5 +7,8 @@ copy /Y LICENSE LICENSE.txt
 call venv\Scripts\activate.bat
 python.exe setup.py bdist_wheel
 twine upload dist/*
+del /Q dictim.egg-info\*.*
+del /Q build\*.*
+del /Q dist\*.*
 call deactivate
 popd
