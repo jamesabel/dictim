3ad89b1dd1e672fc72ba0aa18663bd79cc29568f
JamesAbel <j@abel.co>
2024-12-26 17:45:15 -0800

    delete directories entirely

--- scripts/pypi.bat
+++ scripts/pypi.bat
@@ -7,8 +7,8 @@ copy /Y LICENSE LICENSE.txt
 call venv\Scripts\activate.bat
 python.exe setup.py bdist_wheel
 twine upload dist/*
-del /Q dictim.egg-info\*.*
-del /Q build\*.*
-del /Q dist\*.*
+rmdir /Q /S build
+rmdir /Q /S dist
+rmdir /Q /S dictim.egg-info
 call deactivate
 popd
